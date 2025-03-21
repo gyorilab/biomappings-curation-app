c59a731e8cc4b695b09b4760f7cac128fc4c6777
Mike Anselmi <git@manselmi.com>
2025-03-21 14:36:23 -0400

    Update Postgres DDL

--- init-biomappings-db-postgres.sql
+++ init-biomappings-db-postgres.sql
@@ -5,77 +5,30 @@
 -- DROP DATABASE IF EXISTS biomappings;
 -- CREATE DATABASE biomappings;
 
-DROP SCHEMA IF EXISTS resources;
-CREATE SCHEMA resources;
+DROP SCHEMA IF EXISTS curation_interface;
+CREATE SCHEMA curation_interface;
 
-SET search_path TO resources,"$user",public;
+SET search_path TO curation_interface,"$user",public;
 
-DROP TABLE IF EXISTS curators;
-CREATE TABLE curators (
-    "user" TEXT,
-    orcid TEXT NOT NULL,
-    name TEXT,
-    PRIMARY KEY (orcid)
+DROP DOMAIN IF EXISTS domain_user_id CASCADE;
+CREATE DOMAIN domain_user_id AS TEXT CHECK (
+  VALUE ~ 'orcid:[0-9]{4}(-[0-9]{4}){3}'
 );
 
-DROP TABLE IF EXISTS incorrect;
-CREATE TABLE incorrect (
-    source_prefix TEXT,
-    source_identifier TEXT,
-    source_name TEXT,
-    relation TEXT,
-    target_prefix TEXT,
-    target_identifier TEXT,
-    target_name TEXT,
-    type TEXT,
-    source TEXT,
-    prediction_type TEXT,
-    prediction_source TEXT,
-    prediction_confidence DOUBLE PRECISION
+DROP DOMAIN IF EXISTS domain_non_negative_bigint CASCADE;
+CREATE DOMAIN domain_non_negative_bigint AS BIGINT CHECK (
+  VALUE >= 0
 );
 
-DROP TABLE IF EXISTS mappings;
-CREATE TABLE mappings (
-    source_prefix TEXT,
-    source_identifier TEXT,
-    source_name TEXT,
-    relation TEXT,
-    target_prefix TEXT,
-    target_identifier TEXT,
-    target_name TEXT,
-    type TEXT,
-    source TEXT,
-    prediction_type TEXT,
-    prediction_source TEXT,
-    prediction_confidence DOUBLE PRECISION
+DROP DOMAIN IF EXISTS domain_mapping_classification CASCADE;
+CREATE DOMAIN domain_mapping_classification AS TEXT CHECK (
+  VALUE IN ('broad', 'correct', 'incorrect', 'narrow', 'unsure')
 );
 
-DROP TABLE IF EXISTS predictions;
-CREATE TABLE predictions (
-    source_prefix TEXT,
-    source_identifier TEXT,
-    source_name TEXT,
-    relation TEXT,
-    target_prefix TEXT,
-    target_identifier TEXT,
-    target_name TEXT,
-    type TEXT,
-    confidence DOUBLE PRECISION,
-    source TEXT
-);
-
-DROP TABLE IF EXISTS unsure;
-CREATE TABLE unsure (
-    source_prefix TEXT,
-    source_identifier TEXT,
-    source_name TEXT,
-    relation TEXT,
-    target_prefix TEXT,
-    target_identifier TEXT,
-    target_name TEXT,
-    type TEXT,
-    source TEXT,
-    prediction_type TEXT,
-    prediction_source TEXT,
-    prediction_confidence DOUBLE PRECISION
+DROP TABLE IF EXISTS tbl_mark;
+CREATE TABLE tbl_mark (
+    user_id domain_user_id NOT NULL,
+    line domain_non_negative_bigint NOT NULL,
+    value domain_mapping_classification NOT NULL,
+    PRIMARY KEY (user_id, line)
 );
